2b9d04e32d647529a519016146462187319cdec8
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2020-09-03 18:02:42 +0200

    tried to fix __init__

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="galspec", # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="Tom Bakx (minor edits: Stefanie Brackenhoff)",
     description="Creates sample galaxy spectra in the GHz/THz range",
     long_description=long_description,
