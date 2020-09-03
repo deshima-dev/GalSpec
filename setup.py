658c9fcbe7f4a486e68451235cc6bbc521931fb1
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2020-09-03 17:32:17 +0200

    finished tests

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="galspec", # Replace with your own username
+    version="0.0.1",
+    author="Tom Bakx (minor edits: Stefanie Brackenhoff)",
+    description="Creates sample galaxy spectra in the GHz/THz range",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Stefanie-B/GalSpec",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
