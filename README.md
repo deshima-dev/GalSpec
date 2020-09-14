91bcf4fa35e83b7cfcc9dfe6b3f92380a5156304
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2020-09-14 16:29:23 +0200

    added rescaling of spectral lines

--- README.md
+++ README.md
@@ -17,25 +17,42 @@ It takes the following inputs:
 * **fLow, fHigh** minimum and maximum frequency that will be in the spectrum in units of GHz
 * **numFreqBins** amount of linearly spaced frequency bins at which the spectrum should be evaluated
 * **linewidth** width of spectral lines in units of km/s
-
+* **COlines** 'Kamenetzky' [1] or 'Rosenberg' [2] determines the amplitude of the CO lines, default is Kamenetzky. 
+* **lines** 'Bonato' [3] or 'Spinoglio' [4] determines the amplitude of the remaining spectral lines, default is Bonato
+* **manualrescale** sets whether the lines should be default ratios by Kamenetzky/Rosenberg and Bonato/Spinoglio, or set by user. Options: 
+	- **'False'** default line amplitudes used
+    - **'Absolute'** numpy array of 25 line amplitudes in Jy additive to blackbody emission can be set in rescaleArray
+    - **'Relative'** numpy array of 25 scalars can be set in rescaleArray. The default ratios from Kamenetzky/Rosenberg are multiplied by these scalars prior to addition to the blackbody spectrum.
+* **rescaleArray** rescales the emission lines according to setting in 'manualrescale'. Order of the lines can be found using ```linenames()```
 
 And creates as output:
 * **freqArray** array frequencies in units of GHz
 * **spectrum** array of the flux densities in the spectrum in units of Jy
+
+[1]: http://dx.doi.org/10.3847/0004-637X/829/2/93
+[2]: https://ui.adsabs.harvard.edu/link_gateway/2015ApJ...801...72R/doi:10.1088/0004-637X/801/2/72
+[3]: https://ui.adsabs.harvard.edu/link_gateway/2014MNRAS.438.2547B/doi:10.1093/mnras/stt2375
+[4]: https://ui.adsabs.harvard.edu/link_gateway/2012ApJ...745..171S/doi:10.1088/0004-637X/745/2/171
+
 
 
 * The spectrum can quickly be plotted using the ```plotspectrum()``` function
 
 This function takes the outputs of ```spectrum()``` as an input and creates a plot with axis labels
 
-## Example
+
+
+* The names of the spectral lines and their order for the ```rescaleArray``` in ```spectrum()``` are outputted in a numpy array.
+
+## Examples
+* Default use
 ```
 import galspec
 
 luminosity = 13.7
 z = 4.43
-fLow = 332 #GHz
-fHigh = 377 #GHz
+fLow = 100 #GHz
+fHigh = 900 #GHz
 numFreqBins = 1500
 linewidth = 600
 gal_freq, gal_flux = galspec.spectrum(luminosity, z, fLow, fHigh, numFreqBins, linewidth)
@@ -45,6 +62,43 @@ galspec.plotspectrum(gal_freq, gal_flux)
 
 ![Example](/example_spectrum.png)
 
+* Using the ratios by Rosenberg and Spinoglio
+```
+import galspec
+
+luminosity = 13.7
+z = 4.43
+fLow = 100 #GHz
+fHigh = 900 #GHz
+numFreqBins = 1500
+linewidth = 600
+gal_freq, gal_flux = galspec.spectrum(luminosity, z, fLow, fHigh, numFreqBins, linewidth, 'Rosenberg', 'Spinoglio')
+
+galspec.plotspectrum(gal_freq, gal_flux)
+```
+![Example2](/example_spectrum_RosSpin.png)
+
+* Using manual ratios to only show CO lines
+```
+import galspec
+
+luminosity = 13.7
+z = 4.43
+fLow = 100 #GHz
+fHigh = 900 #GHz
+numFreqBins = 1500
+linewidth = 600
+
+names = galspec.linenames()
+lines = np.zeros(len(names))
+for i in range(len(names)):
+    if names[i].startswith('CO'): lines[i]=1
+
+gal_freq, gal_flux = galspec.spectrum(luminosity, z, fLow, fHigh, numFreqBins, linewidth, manualrescale = 'Relative', rescaleArray = lines)
+
+galspec.plotspectrum(gal_freq, gal_flux)
+```
+![Example3](/example_spectrum_CO_only.png)
 
 ## Installation
 ```pip install galspec```
@@ -53,3 +107,4 @@ galspec.plotspectrum(gal_freq, gal_flux)
 * ```Numpy```
 * ```astropy```
 * ```matplotlib```
+* ```scipy```
--- galspec/spectrumCreate.py
+++ galspec/spectrumCreate.py
@@ -13,6 +13,7 @@ import numpy as np
 from astropy.cosmology import Planck15 as cosmo
 from matplotlib import pyplot as plt
 from pathlib import Path
+from scipy.stats import norm
 
 
 
@@ -101,15 +102,13 @@ def gaus(x,a,x0,sigma):
 
 def LFIRtoSL(Luminosity,z,variance,COlines='Kamenetzky',lines='Bonato',giveNames='No'):
     '''
-    Input: Luminosity [log (Lo)], redshift (z), variance (0 - no OR 1 - yes), COlines (Kamenetzky OR Rosenboom), lines (Bonato OR Spinoglio), giveNames ('Table', 'Tex' OR 'No').
+    Input: Luminosity [log (Lo)], redshift (z), variance (0 - no OR 1 - yes), COlines (Kamenetzky OR Rosenberg), lines (Bonato OR Spinoglio), giveNames ('Table', 'Tex' OR 'No').
     This will produce a list of frequencies [GHz] and flux densities [Jy], with or without a random variance for the SLs:
     Row 0 to 12: CO (1-0) to CO(13-12)
     Row 13 to 17: SIII, SiII, OIII, NIII, OI,
     Row 18 to 24: OIII, NII, OI, CII, CI, CI, NII
     giveNames toggles returning the names of the spectral lines in a list of strings.
     '''
-    import numpy as np
-    from scipy.stats import norm
     Dl = Dlpen(z,giveAnswerInMeters=True)
     Dlmpc = Dlpen(z,giveAnswerInMeters=False)
     c = 3.0e8
@@ -121,10 +120,11 @@ def LFIRtoSL(Luminosity,z,variance,COlines='Kamenetzky',lines='Bonato',giveNames
     pathK17 = Path(__file__).parent / "K17_Table7"
     pathBonato = Path(__file__).parent / "coeffBonato"
     pathSpin = Path(__file__).parent / "coeff_spinoglio"
+    pathRos = Path(__file__).parent / "COcoeff"
     if COlines == 'Kamenetzky':
         slco = np.genfromtxt(pathK17, skip_header=1, dtype=np.float, delimiter=", ", unpack = False)
-    elif COlines == 'Rosenboom':
-        slco = np.loadtxt(pathK17, dtype=np.float, delimiter=" ", unpack = False)
+    elif COlines == 'Rosenberg':
+        slco = np.loadtxt(pathRos, dtype=np.float, delimiter=" ", unpack = False)
     else:
         print('Did not recognise the CO-lines library, will be using Kamenetzky')
         slco = np.genfromtxt(pathK17, skip_header=1, dtype=np.float, delimiter=", ", unpack = False)
@@ -199,7 +199,7 @@ def LFIRtoSL(Luminosity,z,variance,COlines='Kamenetzky',lines='Bonato',giveNames
         for i in range(13,25):
             #outputArray = variance * (lum / f-width) * (10^26 / 4 pi Dl^2)
             outputArray[i,1]= (sl[i-13,2]**outputArray[i,2]) * (lSun*(10**(sl[i-13,0] * Luminosity - sl[i-13,1]))/(df*outputArray[i,0]*1.e9)) * ((10**26)/(Dl*Dl*4.*np.pi))
-    if COlines != 'Rosenboom':
+    if COlines != 'Rosenberg':
         # This method relies on many galaxies, documented in Kamenetzky
         # They use the unwieldy units of Laccent, which with a bit of effort can give the S_CO
         # The amplitude and the steepness are fitted in the paper, and simulated here.
@@ -208,7 +208,7 @@ def LFIRtoSL(Luminosity,z,variance,COlines='Kamenetzky',lines='Bonato',giveNames
             Laccent = (Luminosity - slco[i,2] - outputArray[i,3]*slco[i,3])/(slco[i,0] + outputArray[i,2]*slco[i,1])
             outputArray[i,1] = (10**Laccent) *((1+z)*((115.*(i+1))**2))/((3.25e7)*(velocity)*(Dlmpc*Dlmpc))
     else:
-        # From Rosenboom's paper, we extract the ratios in luminosity of each line, and then relate that to the CII luminosity
+        # From Rosenberg's paper, we extract the ratios in luminosity of each line, and then relate that to the CII luminosity
         # The variation is exactly that of the CII-line. The CII / CO (1-0) relation is from Stacey.
         # There is no scatter inside the CO-ladder
         for i in range(13):
@@ -229,15 +229,35 @@ def LFIRtoSL(Luminosity,z,variance,COlines='Kamenetzky',lines='Bonato',giveNames
     else:
         return outputArray
 
-def spectrum(luminosity,redshift,fLow=220,fHigh=440,numFreqBins = 1500,linewidth=300):
+def spectrum(luminosity,redshift,fLow=220,fHigh=440,numFreqBins = 1500,linewidth=300,COlines='Kamenetzky',lines='Bonato',manualrescale = 'False', rescaleArray = []):
     """
     Creates a galaxy spectrum consisting of a blackbody continuum and spectral
     lines. Keywords:
     luminosity: in units of log(L_fir [L_sol])
     redshift: z
-    fLow, fHigh: minimum and maximum frequency that will be in the spectrum in units of GHz
-    numFreqBins: amount of linearly spaced frequency bins at which the spectrum should be evaluated
+    fLow, fHigh: minimum and maximum frequency that will be in the spectrum in 
+                 units of GHz
+    numFreqBins: amount of linearly spaced frequency bins at which the spectrum 
+                 should be evaluated
     linewidth: width of spectral lines in units of km/s
+    COlines: 'Kamenetzky' or 'Rosenberg' determines the amplitude of the 
+              CO lines, default is Kamenetzky
+    lines: 'Bonato' or 'Spinoglio' determines the amplitude of the remaining 
+           spectral lines, default is Bonato
+    manualrescale: sets whether the lines should be default ratios by 
+                   Kamenetzky/Rosenberg and Bonato/Spinoglio, or set by user
+                   options: 'False' - default line amplitudes used
+                            'Absolute' - numpy array of 25 line amplitudes in 
+                                        Jy additive to blackbody emission can 
+                                        be set in rescaleArray
+                            'Relative' - numpy array of 25 scalars can be set 
+                                        in rescaleArray. The default ratios 
+                                        from Kamenetzky/Rosenberg are 
+                                        multiplied by these scalars prior 
+                                        to addition to the blackbody spectrum.
+    rescaleArray: rescales the emission lines according to setting in 
+                  'manualrescale'. Order of the lines can be found using 
+                  linenames()
     # Output: freqArray -> array frequencies in units of GHz
     # Output: spectrum -> array of the flux densities in the spectrum in units of Jy
     """
@@ -250,10 +270,22 @@ def spectrum(luminosity,redshift,fLow=220,fHigh=440,numFreqBins = 1500,linewidth
     normLum = giveLuminosity(np.array([spectrum[0],spectrum[0]]),np.array([1,1]),((3.e8)/((1.e9)*np.array([freqArray[0],freqArray[0]]))),redshift,T_cold,T_hot,Ratio,Beta)
     spectrum = (10.**luminosity)*spectrum/normLum
 	# Add the spectrum lines
-    B,names = LFIRtoSL(luminosity,redshift,0,giveNames='Table')
-    for i in range(len(B)):
-        specLine = gaus(freqArray,B[i,1]*(600/linewidth),B[i,0],B[i,0]*linewidth/(3.e5))
-        spectrum += specLine
+    B = LFIRtoSL(luminosity,redshift,0,COlines,lines)
+    if manualrescale == 'Absolute':
+        for i in range(len(B)):
+            specLine = gaus(freqArray,rescaleArray[i],B[i,0],B[i,0]*linewidth/(3.e5))
+            spectrum += specLine
+    elif manualrescale == 'Relative':
+        for i in range(len(B)):
+            specLine = gaus(freqArray,rescaleArray[i]*B[i,1]*(600/linewidth),B[i,0],B[i,0]*linewidth/(3.e5))
+            spectrum += specLine
+    elif manualrescale != 'False':
+        print('Invalid keyword for manualrescale. Assuming \'False\'')
+        manualrescale = 'False'
+    if manualrescale == 'False':
+        for i in range(len(B)):
+            specLine = gaus(freqArray,B[i,1]*(600/linewidth),B[i,0],B[i,0]*linewidth/(3.e5))
+            spectrum += specLine
     return freqArray,spectrum
 
 def plotspectrum(freqArray, spectrumArray):
@@ -270,3 +302,10 @@ def plotspectrum(freqArray, spectrumArray):
     plt.xlim(np.min(freqArray), np.max(freqArray))
     plt.ylim(bottom=0)
     plt.show()
+    
+def linenames():
+    """
+    Returns the names of the spectral lines from spectrum() in order in a numpy array
+    """
+    B, names= LFIRtoSL(12,1,0, giveNames = 'Table')
+    return np.array(names)
